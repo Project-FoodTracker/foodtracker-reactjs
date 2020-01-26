63393a6f414958ca9255fbfa982e615d53152c72
Armin Winkler <armin.winkler96@gmail.com>
2020-01-26 19:01:44 +0100

    Add scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/index.css';
+import './assets/css/index.scss';
 import App from './components/App';
 import * as serviceWorker from './utils/serviceWorker';
 
